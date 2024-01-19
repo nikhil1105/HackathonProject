04ba49c301f265ddef366f536c67396d023f9c30
ArunGithub23 <garun2161@gmail.com>
2024-01-19 13:18:53 +0530

    registration page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {  } from "module";
 import { BrowserRouter,Routes , Route } from "react-router-dom";
 import Landing from "./componenets/landing";
+import { Registration } from "./componenets/registration";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="landing" element={<Landing/>}/>
+        <Route path="register" element={<Registration/>}/>
 
       </Routes>
     </BrowserRouter>
---
+++ src/componenets/registration.jsx
@@ -0,0 +1,46 @@
+export const Registration = () => {
+  return (
+    <div className="main m-auto">
+      <div className="part1">
+        <h1>Welcome TO The </h1>
+      </div>
+      <div className="part2">
+        <label>Name</label>
+        <br />
+        <input type="text" name="" id="" placeholder="First" />
+        <input type="text" name="" id="" placeholder="Last" />
+        <br />
+        <label>Date of Birth</label>
+        <br />
+        <input type="text" name="" id="" placeholder="MM/DD/YYYY" />
+        <br />
+        <label>Contact Phone Number</label>
+        <br />
+        <input type="text" name="" id="" placeholder="" />
+        <br />
+        <label>Email Address</label>
+        <br />
+
+        <input type="text" name="" id="" placeholder="" />
+        <br />
+
+        <label>Type(s) Of Substance(s) Abused</label>
+        <br />
+
+        <input type="text" name="" id="" placeholder="" />
+        <br />
+        <label >Frequency Of Use</label><br />
+
+<input type="number" name="" id="" placeholder="" /><br />
+<label >Date Of Last Use</label><br />
+
+<input type="text" name="" id="" placeholder="" /><br />
+
+
+
+
+      </div>
+          
+    </div>
+  );
+};
